76816aa0f914ba3fd131917524bd7e1758b44363
Your Name <22282176@CFP127ALUNO.LOCAL>
2022-09-01 16:59:44 -0300

    Declaração de variáveis tabuada

--- src/br/senai/jandira/sp/AppTabuada.java
+++ src/br/senai/jandira/sp/AppTabuada.java
@@ -1,9 +1,17 @@
 package br.senai.jandira.sp;
 
+import javax.swing.JOptionPane;
+
 public class AppTabuada {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declaração de variáveis 
+		
+		int número = 0;
+		int tabuada = 0; 
+		
+		número = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+	    for (int i = 1; i<= 10; i++);
 
 	}
 
