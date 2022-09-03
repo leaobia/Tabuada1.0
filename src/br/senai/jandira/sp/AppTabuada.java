2ea162589c0455ba5ff8e23fc1db812ee7e7562a
unknown <biancapeleao@gmail.com>
2022-09-03 09:20:38 -0300

    Iniciando Interface Gráfica

--- src/br/senai/jandira/sp/AppTabuada.java
+++ src/br/senai/jandira/sp/AppTabuada.java
@@ -8,10 +8,13 @@ public class AppTabuada {
 	public static void main(String[] args) {
 		
 		FrameTabuada tela = new FrameTabuada();
-		tela.titulo = "Tabuada 1.0";
+		tela.titulo = "Tabuada";
 		tela.altura = 600;
 		tela.largura = 500;
 		tela.criarTela(); 
+		
+		
+    // Testando Cálculo Matemático 
 		
 	    
 	Tabuada tabuada2 = new Tabuada ();
--- src/br/senai/jandira/sp/gui/FrameTabuada.java
+++ src/br/senai/jandira/sp/gui/FrameTabuada.java
@@ -1,6 +1,11 @@
 package br.senai.jandira.sp.gui;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import java.awt.Container;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JLabel;
 
 public class FrameTabuada {
@@ -14,19 +19,25 @@ public class FrameTabuada {
 	public void criarTela (){
 	
 	JFrame tela = new JFrame();
+	
+	BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+    tela.setIconImage(image);
 	tela.setTitle(titulo);
     tela.setSize(largura, altura);
+    tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	tela.setLayout(null);
 	tela.getContentPane();
+	
 	
 	// Criação Container 
 	
 	Container painel = tela.getContentPane();	
 	
-	// Criação dos componentes da tela 
+	// Criação dos componentes da tela
 	
 	JLabel labelTitulo = new JLabel();
 	labelTitulo.setText("Tabuada");
+	BufferedImage icon = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
 	
 	
 	
@@ -35,6 +46,12 @@ public class FrameTabuada {
 	painel.setVisible(true);
 	tela.setVisible(true);
 	labelTitulo.setVisible(true);
+	
+	
+	
+	
+	
+	
 	
 	}
 }
